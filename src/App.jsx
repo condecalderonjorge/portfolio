673afc2166ac8717de5defe47478b60938d85ab6
Jorge Conde Calderon <jorgecondecalderon@MacBook-Air-de-Jorge.local>
2025-11-30 14:20:17 +0100

    github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Hero from './components/Hero.jsx'
+import Hero from './components/hero.jsx'
 import Stack from './components/Stack.jsx'
 import Projects from './components/projects.jsx'
 import About from './components/About.jsx'
 import Contact from './components/Contact.jsx'
-import Footer from './components/Footer.jsx'
+import Footer from './components/footer.jsx'
 
 function App() {
   return (
